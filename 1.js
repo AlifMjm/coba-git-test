fb535d0d45c059c753a750aa3755eccfa2bf67d8
AlifMjm <alifmustiko@gmail.com>
2025-12-11 18:52:10 +0700

    first push

---
+++ 1.js
@@ -0,0 +1,30 @@
+const umur = 15;
+const jenisKursi = "PREMIUM";
+
+let hargaTiket = 0;
+let tambahanKursi = 0;
+
+if (umur >= 18) {
+    hargaTiket = 50000;
+} else if (umur >= 12) {
+    hargaTiket = 35000;
+} else {
+    hargaTiket = 25000;
+}
+
+switch (jenisKursi) {
+    case "VIP":
+        tambahanKursi = 20000;
+        break;
+    case "PREMIUM":
+        tambahanKursi = 10000;
+        break;
+    case "REGULAR":
+        tambahanKursi = 0;
+        break;
+}
+
+// Hitung totalnya
+const total = hargaTiket + tambahanKursi;
+
+console.log("Total harga:", total);
